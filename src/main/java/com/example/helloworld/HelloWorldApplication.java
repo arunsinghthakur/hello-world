3c41465fe9242eb1266edd1e5525cdd67b8d6bdd
Arun Singh <arun_gkv@yahoo.co.in>
2019-04-22 12:00:43 +0530

    docker file

--- src/main/java/com/example/helloworld/HelloWorldApplication.java
+++ src/main/java/com/example/helloworld/HelloWorldApplication.java
@@ -1,5 +1,7 @@
 package com.example.helloworld;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,12 +11,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloWorldApplication {
 
+	private static final Logger logger = LoggerFactory.getLogger(HelloWorldApplication.class);
+
 	public static void main(String[] args) {
+
 		SpringApplication.run(HelloWorldApplication.class, args);
 	}
 
 	@GetMapping
 	public String sayHello() {
+		logger.info("New request has been received." + System.currentTimeMillis());
 		return "Hello world. JOKER";
 	}
 
